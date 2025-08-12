96a792f370ef6f8ce7a48100959b8830282a5595
asamarsal <ASAMARSAL@gmail.com>
2025-08-13 01:49:06 +0700

    Add

--- fitlog/components/reactbits/typetext.jsx
+++ fitlog/components/reactbits/typetext.jsx
@@ -17,7 +17,7 @@ const TextType = ({
   cursorCharacter = "|",
   cursorClassName = "",
   cursorBlinkDuration = 0.5,
-  textColors = [],
+  textColors = string[],
   variableSpeed,
   onSentenceComplete,
   startOnVisible = false,
