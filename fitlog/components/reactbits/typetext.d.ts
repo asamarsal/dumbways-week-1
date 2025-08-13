4a3b26aaa839dcb4c6adaa09ec18f2ce7d9d5536
asamarsal <ASAMARSAL@gmail.com>
2025-08-13 07:27:06 +0700

    Test

---
+++ fitlog/components/reactbits/typetext.d.ts
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+interface TextTypeProps {
+  text: string | string[];
+  as?: string;
+  typingSpeed?: number;
+  initialDelay?: number;
+  pauseDuration?: number;
+  deletingSpeed?: number;
+  loop?: boolean;
+  className?: string;
+  showCursor?: boolean;
+  hideCursorWhileTyping?: boolean;
+  cursorCharacter?: string;
+  cursorClassName?: string;
+  cursorBlinkDuration?: number;
+  textColors?: string[];
+  variableSpeed?: { min: number; max: number };
+  onSentenceComplete?: (text: string, index: number) => void;
+  startOnVisible?: boolean;
+  reverseMode?: boolean;
+  [key: string]: any;
+}
+
+declare function TextType(props: TextTypeProps): ReactNode;
+
+export default TextType;
